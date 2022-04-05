1d0fd70e605c3d6890a8df4b10c637ea317b4eea
Siddhesh Tawde <siddheshtawde35@gmail.com>
2022-04-05 23:02:01 +0530

    Service Worker Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import { lazy, Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { auth } from './firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth'
-
-import SignIn from './components/Authentication/SignIn';
-import Home from './components/Home/Home';
-import Conversation from './components/Conversation/Conversation';
-import AddUser from './components/AddUser/AddUser';
-
 import Global from './components/Loaders/Global';
+
+const SignInPage = lazy(() => import('./components/Authentication/SignIn'));
+const HomePage = lazy(() => import('./components/Home/Home'));
+const ConversationPage = lazy(() => import('./components/Conversation/Conversation'));
+const AddUserPage = lazy(() => import('./components/AddUser/AddUser'));
 
 function App() {
   const [user, loading, error] = useAuthState(auth);
@@ -18,11 +18,11 @@ function App() {
 
   return (
     <Routes>
-      <Route path='/sign-in' element={user ? <Navigate to='/' /> : <SignIn />} />
+      <Route path='/sign-in' element={user ? <Navigate to='/' /> : <Suspense fallback={<Global />}><SignInPage /></Suspense>} />
 
-      <Route path='/add' element={user ? <AddUser /> : <Navigate to='/sign-in' />} />
-      <Route path='/conversation/:id' element={user ? <Conversation /> : <Navigate to='/sign-in' />} />
-      <Route path='/' element={user ? <Home /> : <Navigate to='/sign-in' />} />
+      <Route path='/add' element={user ? <Suspense fallback={<Global />}><AddUserPage /></Suspense> : <Navigate to='/sign-in' />} />
+      <Route path='/conversation/:id' element={user ? <Suspense fallback={<Global />}><ConversationPage /></Suspense> : <Navigate to='/sign-in' />} />
+      <Route path='/' element={user ? <Suspense fallback={<Global />}><HomePage /></Suspense> : <Navigate to='/sign-in' />} />
 
     </Routes>
   )
